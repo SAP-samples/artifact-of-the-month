da5b3007ab8b3c27fc450adfc6f0a1d5fc100029
Marius Obert <Obatzer@gmail.com>
2021-08-20 09:17:27 +0200

    test pypi setup

--- analyzer/src/providers/index.ts
+++ analyzer/src/providers/index.ts
@@ -3,5 +3,5 @@ import ghStars from "./gh-repo";
 import npm from "./npm-downloads";
 import pypi from "./pypi";
 
-// export default [npm];
-export default [dh, npm, ghStars]; // remove pypi for now , pypi];
+export default [pypi];
+// export default [dh, npm, ghStars, pypi];
