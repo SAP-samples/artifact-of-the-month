24fda64b65d910f5633694ceeb8fac58e8316565
marianfoo <13335743+marianfoo@users.noreply.github.com>
2021-11-24 19:56:05 +0100

    fix: move updated github data directly to docs
    Signed-off-by: marianfoo <13335743+marianfoo@users.noreply.github.com>

--- analyzer/src/update-gh.ts
+++ analyzer/src/update-gh.ts
@@ -79,8 +79,8 @@ import providers from "./providers";
 
     // write new data to frontend data
     writeFileSync(
-        `${__dirname}/../../frontend/trends/webapp/model/allItems.json`,
+        `${__dirname}/../../docs/model/allItems.json`,
         JSON.stringify(currentAllItemsJson)
     );
-    writeFileSync(`${__dirname}/../../frontend/trends/webapp/model/trends.json`, JSON.stringify(currentTrendsJson));
+    writeFileSync(`${__dirname}/../../docs/model/trends.json`, JSON.stringify(currentTrendsJson));
 })();
