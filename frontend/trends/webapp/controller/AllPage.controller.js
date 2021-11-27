e5f0b3eaf9cd1f19a7d7dfb3e95e5dae3636e3ae
marianfoo <13335743+marianfoo@users.noreply.github.com>
2021-11-27 19:26:38 +0100

    feat(frontend): add bookmarking for search/filter/sort
    Signed-off-by: marianfoo <13335743+marianfoo@users.noreply.github.com>

--- frontend/trends/webapp/controller/AllPage.controller.js
+++ frontend/trends/webapp/controller/AllPage.controller.js
@@ -6,12 +6,55 @@ sap.ui.define(
         return Controller.extend("aow.artifact.controller.AllPage", {
             formatter: formatter,
 
+            onInit: function () {
+                this.oRouter = this.getRouter();
+                this._oRouterArgs = null;
+                // make the search bookmarkable
+                this.oRouter.getRoute("all").attachMatched(this._onRouteMatched, this);
+            },
+
+
+            _onRouteMatched: function (oEvent) {
+                // save the current query state
+                this._oRouterArgs = oEvent.getParameter("arguments");
+                this._oRouterArgs["?query"] = this._oRouterArgs["?query"] || {};
+
+                // search/filter via URL hash
+                if (this._oRouterArgs["?query"].search || this._oRouterArgs["?query"].filterType) {
+                    this._applySearchFilter(this._oRouterArgs["?query"].search, this._oRouterArgs["?query"].filterType);
+                }
+                if (this._oRouterArgs["?query"].sortKey && this._oRouterArgs["?query"].descending) {
+                    this._applySorting(this._oRouterArgs["?query"].sortKey, this._oRouterArgs["?query"].descending);
+                }
+            },
+
             liveSearch: function (oEvent) {
                 const model = this.getView().getModel("settings");
                 let value = model.getProperty("/search");
-                if (!value) { value = "" }
                 let valueType = model.getProperty("/filterSearch");
+                this._applySearchFilter(value, valueType);
+
+                // update the hash with the current search term
+                if ( value !== undefined && value.length > 0 ) {
+                    this._oRouterArgs["?query"].search = value;
+                } else {
+                    // delete empty search term from hash
+                    if (this._oRouterArgs["?query"].hasOwnProperty("search")) {
+                        delete this._oRouterArgs["?query"].search;
+                    }
+                }
+                if (valueType !== undefined && valueType.length > 0 ) {
+                    this._oRouterArgs["?query"].filterType = valueType;
+                }
+                this.oRouter.navTo("all", this._oRouterArgs, true /*no history*/);
+            },
+
+            _applySearchFilter: function (value, valueType) {
+                if (!value) { value = "" }
                 if (valueType === undefined || valueType === 'all') { valueType = "" }
+                // make sure values are set in model
+                this.getView().getModel("settings").setProperty("/search", value);
+                this.getView().getModel("settings").setProperty("/filterSearch", valueType);
                 const list = this.getView().byId("all-list");
                 const listBinding = list.getBinding("items");
                 const nameFilter = new Filter({
@@ -63,11 +106,22 @@ sap.ui.define(
 
             // apply sorting parameters
             handleConfirm: function (oEvent) {
+                var key = oEvent.getParameter("sortItem").getKey();
+                var descending = oEvent.getParameter("sortDescending")
+                this._applySorting(key, descending);
+                this._oRouterArgs["?query"].sortKey = key;
+                this._oRouterArgs["?query"].descending = descending;
+                this.oRouter.navTo("all", this._oRouterArgs, true /*no history*/);
+
+            },
+
+            _applySorting: function (key, descending) {
+
                 const listBinding = this.getView().byId("all-list").getBinding("items");
                 const oSorter = new Sorter({
-                    path: oEvent.getParameter("sortItem").getKey(), 
-                    descending: oEvent.getParameter("sortDescending"),
-                  }); 
+                    path: key,
+                    descending: descending,
+                });
                 listBinding.sort(oSorter);
             }
         });
